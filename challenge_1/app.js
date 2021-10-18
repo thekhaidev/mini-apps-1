201d4df04effb4a6fff0b7869c87073b6ed86ca6
Khai Tran <kqtran917@yahoo.com>
2021-10-18 14:17:00 -0500

    Implemented alternating turns

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,10 +1,26 @@
+var turn = 'First'
+
 function testClick(e) {
- console.log(e.path[0].innerHTML)
- if (e.path[0].innerHTML === "[X]") {
-   e.path[0].innerHTML = "[O]"
- } else {
-  e.path[0].innerHTML = "[X]"
+//  console.log(e.path[0].innerHTML)
+
+ if ((e.path[0].innerHTML === "[  ]") && (turn === 'First')) {
+  //  console.log('Current turn: ', turn)
+   e.path[0].innerHTML = "[X]"
+   console.log('Current turn: ', turn)
+   turn = 'O'
  }
+
+ if ((e.path[0].innerHTML === "[  ]") && (turn === 'O')) {
+  console.log('Currently turn ', turn)
+  e.path[0].innerHTML = "[O]"
+  turn = 'X'
+}
+
+ if ((e.path[0].innerHTML === "[  ]") && (turn === 'X')){
+  console.log('Currently turn ', turn)
+  e.path[0].innerHTML = "[X]"
+  turn = 'O'
+}
 }
 
 const board = document.getElementById("board");
