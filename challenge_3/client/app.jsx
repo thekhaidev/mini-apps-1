1f607defa276c7341e4081602dfc73fa1454b976
Khai Tran <kqtran917@yahoo.com>
2021-10-20 15:11:50 -0500

    Implemented skeleton

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -44,7 +44,7 @@ class Test extends React.Component {
   }
 }
 
-class AnotherTest extends React.Component {
+class Form1 extends React.Component {
   constructor(props) {
     super(props)
   }
@@ -52,7 +52,20 @@ class AnotherTest extends React.Component {
   render() {
     return (
       <div>
-        <h1>AHA. GOT EM</h1>
+        <form>
+          <label>Name:</label>
+          <input></input>
+          <br />
+          <label>Email</label>
+          <input></input>
+          <br />
+          <label>Password</label>
+          <input></input>
+        </form>
+        <br />
+        <button onClick={homePage}>Back</button>
+        <button onClick={nextClick}>Next</button>
+
       </div>
     )
   }
@@ -60,8 +73,83 @@ class AnotherTest extends React.Component {
 
 }
 
+class Form2 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Address:</label>
+          <input></input>
+          <br />
+          <label>Line 1:</label>
+          <input></input>
+          <br />
+          <label>Line2:</label>
+          <input></input>
+          <label>City:</label>
+          <input></input>
+          <br />
+          <label>State:</label>
+          <input></input>
+          <br />
+          <label>Zipcode</label>
+          <input></input>
+        </form>
+        <br />
+        <button onClick={testClick}>Back</button>
+        <button onClick={thirdClick}>Next</button>
+
+
+      </div>
+    )
+  }
+}
+
+class Form3 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Credit Card:</label>
+          <input></input>
+          <br />
+          <label>Expiry:</label>
+          <input></input>
+          <br />
+          <label>CVV:</label>
+          <input></input>
+          <label>Billing Zip:</label>
+          <input></input>
+        </form>
+        <br />
+        <button onClick={nextClick}>Back</button>
+      </div>
+    )
+  }
+}
 const testClick = (props) => {
-  ReactDOM.render(<AnotherTest />, document.getElementById('root'))
+  ReactDOM.render(<Form1 />, document.getElementById('root'))
+}
+
+const nextClick = (props) => {
+  ReactDOM.render(<Form2 />, document.getElementById('root'))
+}
+
+const thirdClick = (props) => {
+  ReactDOM.render(<Form3 />, document.getElementById('root'))
+}
+
+
+const homePage = (props) => {
+  ReactDOM.render(<App />, document.getElementById('root'))
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
