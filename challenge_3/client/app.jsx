be27c84e7a49b77a91b01f5a64e07e886ad531ea
Khai Tran <kqtran917@yahoo.com>
2021-10-20 14:41:48 -0500

    Multiple components in one file test

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -7,9 +7,26 @@ class App extends React.Component {
     return (
       <div>
         <h1>Yo this is an app</h1>
+        <Test />
       </div>
     )
   }
 }
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        This is another test
+      </div>
+    )
+  }
+}
+
+
 
 ReactDOM.render(<App/>, document.getElementById('root'))
